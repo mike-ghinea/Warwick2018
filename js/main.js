5a25f7bf17dda04e6585a50d90dafbb9cb70fc40
Alex Dumitru <alexadrian.dmtr@gmail.com>
2018-02-18 02:05:45 +0000

    Polish UI

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-$(document).ready(function() {
-    // Initialize Firebase
-    var config = {
-        authDomain: "rubberduck-69fdc.firebaseapp.com",
-        databaseURL: "https://rubberduck-69fdc.firebaseio.com",
-        projectId: "rubberduck-69fdc",
-    };
-    firebase.initializeApp(config);
-    var database = firebase.database();
-    var solved = database.ref('numberOfProblemsSolved');
-    solved.on('value', data => updateSolved(data.val()));
-    function updateSolved(numberOfSolved) {
-        $("#numberOfSolved").text(numberOfSolved);
-    }
+var config = {
+    authDomain: "rubberduck-69fdc.firebaseapp.com",
+    databaseURL: "https://rubberduck-69fdc.firebaseio.com",
+    projectId: "rubberduck-69fdc",
+};
 
+var database;
+var refs = { solved: null, notSolved: null };
+$(document).ready(function() {
+    firebase.initializeApp(config);
+    database = firebase.database();
+    refs.solved = database.ref('numberOfProblemsSolved');
+    refs.notSolved = database.ref('numberOfProblemsNotSolved');
+    refs.solved.on('value', data => updateSolved(data.val()));
 });
+
+function updateSolved(numberOfSolved) {
+    $("#numberOfSolved").text(numberOfSolved);
+}
+
+function incrementRef(ref) {
+    ref.on('value', data => {
+        var oldNumber = data.val();
+        ref.set(oldNumber+1);
+    })
+}
+
+function yes() {
+    incrementRef(refs.solved);
+}
+
+function no() {
+    incrementRef(refs.notSolved);
+}
