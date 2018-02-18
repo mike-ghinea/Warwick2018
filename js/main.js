6b581ff0a9e2c0a095819fd9e3787e33564f5e73
Alex Dumitru <alexadrian.dmtr@gmail.com>
2018-02-18 01:22:31 +0000

    Re-add ruber duck

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+    // Initialize Firebase
+    var config = {
+        authDomain: "rubberduck-69fdc.firebaseapp.com",
+        databaseURL: "https://rubberduck-69fdc.firebaseio.com",
+        projectId: "rubberduck-69fdc",
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    var solved = database.ref('numberOfProblemsSolved');
+    solved.on('value', data => updateSolved(data.val()));
+    function updateSolved(numberOfSolved) {
+        $("#numberOfSolved").text(numberOfSolved);
+    }
 
+});
